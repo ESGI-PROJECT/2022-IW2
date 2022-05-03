3e063c6adf9772b2e774f51436c8c932da7ccf95
Syu <herve.tutuaku@beamy.io>
2022-05-03 20:11:17 +0200

    indexed db

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import page from "page";
+import checkConnectivity from "network-latency";
+import { getRessource, getRessources, setRessource, setRessources } from './idbHelper';
 import { getProducts, getProduct } from "./api/products"
 
 
 (async (root) => {
   const skeleton = root.querySelector(".skeleton");
   const main = root.querySelector("main");
+
+  checkConnectivity({
+    interval: 3000,
+    threshold: 2000
+  });
+
+  let NETWORK_STATE = true;
+
+  document.addEventListener('connection-changed', ({ detail: state }) => {
+    NETWORK_STATE = state;
+    if (NETWORK_STATE) {
+      document.documentElement
+        .style.setProperty('--app-bg-color', 'royalblue');
+    } else {
+      document.documentElement
+        .style.setProperty('--app-bg-color', '#6e6f72');
+    }
+  });
 
   const AppHome = main.querySelector('app-home');
   const AppProduct = main.querySelector('app-product');
@@ -20,8 +40,16 @@ import { getProducts, getProduct } from "./api/products"
 
   page('/', async () => {
     await import("./views/app-home");
-    const products = await getProducts();
-    AppHome.products = products;
+    
+    let storedProducts = [];
+    if (NETWORK_STATE) {
+      const products = await getProducts();
+      storedProducts = await setRessources(products);
+    } else {
+      storedProducts = await getRessources();
+    }
+
+    AppHome.products = storedProducts;
     AppHome.active = true;
 
     skeleton.setAttribute('hidden', true);
@@ -29,9 +57,16 @@ import { getProducts, getProduct } from "./api/products"
 
   page('/product/:id', async ({ params }) => {
     await import('./views/app-product');
-    const product = await getProduct(params.id);
 
-    AppProduct.product = product;
+    let storedProduct = {};
+    if (NETWORK_STATE) {
+      const product = await getProduct(params.id);
+      storedProduct = await setRessource(product);
+    } else {
+      storedProduct = await getRessource(params.id);
+    }
+
+    AppProduct.product = storedProduct;
     AppProduct.active = true;
 
     skeleton.setAttribute('hidden', true);
